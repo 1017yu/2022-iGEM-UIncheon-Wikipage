46a9b16522bded708f7d4e71f529687f53b22e61
Kled <1017yu@daum.net>
2022-07-26 18:32:55 +0900

    Update

--- static/main.js
+++ static/main.js
@@ -1,6 +1,7 @@
 const badgeEl = document.getElementById("uibadges");
 const menu = document.getElementById("inner");
 const hiddenDiv = document.getElementById("gnb-white");
+const titleholder = document.querySelector("title-holder");
 // const toTopEl = document.querySelector("#to-top");
 // 페이지에 스크롤 이벤트를 추가!
 // 스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 줌)
@@ -43,10 +44,22 @@ window.addEventListener(
 // });
 
 menu.addEventListener("mouseover", function handleMouseOver() {
-  hiddenDiv.style.height = "770px";
+  hiddenDiv.style.height = "776px";
   hiddenDiv.style.transition = "height 0.4s ease";
 });
 
 menu.addEventListener("mouseout", function handleMouseOut() {
   hiddenDiv.style.height = "0";
+});
+
+const fadeEls = document.querySelectorAll(".title-holder .txt-mask");
+// 나타날 요소들을 하나씩 반복해서 처리!
+fadeEls.forEach(function (fadeEl, index) {
+  // 각 요소들을 순서대로(delay) 보여지게 함!
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.3,
+    y: 10,
+    x: 10,
+    opacity: 1,
+  });
 });
