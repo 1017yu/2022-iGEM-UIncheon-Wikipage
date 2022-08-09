82eeb8ac52d0553f36af3d82b798ca8fd2bc6db4
Kled <1017yu@daum.net>
2022-08-09 12:02:26 +0900

    Update

--- static/main.js
+++ static/main.js
@@ -140,6 +140,67 @@ window.addEventListener(
   }, 80)
 );
 
+const fadeEl__6 = document.querySelectorAll(".row-1-1");
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    if (window.scrollY > 2300) {
+      fadeEl__6.forEach(function (fadeEl, index) {
+        gsap.to(fadeEl, 1, {
+          delay: index * 4.5,
+          opacity: 1,
+        });
+      });
+    } else {
+    }
+  }, 80)
+);
+
+const fadeEl__7 = document.querySelectorAll(".row-1-2-img");
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    if (window.scrollY > 2700) {
+      fadeEl__7.forEach(function (fadeEl, index) {
+        gsap.to(fadeEl, 1, {
+          delay: index * 0.5,
+          opacity: 0,
+        });
+      });
+    } else if (2700 < window.scrollY < 2900) {
+      fadeEl__7.forEach(function (fadeEl, index) {
+        gsap.to(fadeEl, 1, {
+          delay: index * 0.5,
+          opacity: 1,
+        });
+      });
+    }
+  }, 80)
+);
+
+const fadeEl__8 = document.querySelectorAll(".row-1-3-img");
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    if (window.scrollY > 3100) {
+      fadeEl__8.forEach(function (fadeEl, index) {
+        gsap.to(fadeEl, 1, {
+          delay: index * 4.5,
+          opacity: 0,
+        });
+      });
+      // 페이지 스크롤 위치가 500px이 넘지 않으면.
+    } else if (window.scrollY < 2900) {
+      fadeEl__8.forEach(function (fadeEl, index) {
+        gsap.to(fadeEl, 1, {
+          delay: index * 4.5,
+          opacity: 1,
+        });
+      });
+    }
+  }, 80)
+);
+
 function random(min, max) {
   return parseFloat((Math.random() * (max - min) + min).toFixed(2));
 }
@@ -150,7 +211,7 @@ function floatingObject(selector, delay, size) {
     random(1.5, 2.5), // 애니메이션 동작 시간
     {
       delay: random(0, delay), // 얼마나 늦게 애니메이션을 시작할 것인지 지연 시간을 설정.
-      y: size, // `transform: translateY(수치);`와 같음. 수직으로 얼마나 움직일지 설정.
+      y: size, // `transform: translateY(수치);`와 같음. 수직으로 얼마나 움직일지 x설정.
       repeat: -1, // 몇 번 반복하는지를 설정, `-1`은 무한 반복.
       yoyo: true, // 한번 재생된 애니메이션을 다시 뒤로 재생.
       ease: Power2.easeInOut, // Easing 함수 적용.
@@ -162,7 +223,7 @@ floatingObject(".sec1-bg", 1, 17);
 (function () {
   init();
 
-  var g_containerInViewport;
+  let g_containerInViewport;
   function init() {
     setStickyContainersSize();
     bindEvents();
@@ -201,9 +262,9 @@ floatingObject(".sec1-bg", 1, 17);
       return;
     }
 
-    var isPlaceHolderBelowTop =
+    let isPlaceHolderBelowTop =
       containerInViewPort.offsetTop < document.documentElement.scrollTop;
-    var isPlaceHolderBelowBottom =
+    let isPlaceHolderBelowBottom =
       containerInViewPort.offsetTop + containerInViewPort.offsetHeight >
       document.documentElement.scrollTop;
     let g_canScrollHorizontally =
@@ -215,3 +276,21 @@ floatingObject(".sec1-bg", 1, 17);
     }
   }
 })();
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerWidth;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
